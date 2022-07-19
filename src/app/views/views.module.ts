5f7d3f5ee2ac2f89c21d8e28cb1ab946ac6db2c0
Kyle Bussert <kyle.bussert@gmail.com>
2022-07-19 13:41:27 -0400

    Implementing reusable Statistics and ItemList components from SemanticUI

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { SharedModule } from '../shared/shared.module';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
   declarations: [
     ViewsHomeComponent,
-    StatisticsComponent
+    StatisticsComponent,
+    ItemListComponent
   ],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    SharedModule
   ]
 })
 export class ViewsModule { }
